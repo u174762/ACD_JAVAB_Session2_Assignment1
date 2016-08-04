a60dc403d11c11b47ef21473c9c23a50f9682afe
u174762 <sam.panga@hotmail.com>
2016-08-04 21:29:29 +0530

    Add files via upload

---
+++ ACD_javab_Session2_assignment1/src/session2dec/ACD_javaB_session2_Assignment1.java
@@ -0,0 +1,24 @@
+
+
+	package session2dec;
+
+	public class ACD_javaB_session2_Assignment1 {
+		public static void main(String[] args){
+			
+	int age=17;
+
+	if(age==18){
+		System.out.println("eligible to vote;");
+		
+	}
+	else if(age>=18){
+		System.out.println("eligible to vote");
+
+		
+	}
+	else if (age<=18){
+		System.out.println("not eligible to vote");
+	}
+
+		}
+		}
